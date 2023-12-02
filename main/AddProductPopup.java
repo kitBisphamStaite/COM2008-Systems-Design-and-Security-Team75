8ef1166f516833d7cfbf15e44c14b06521aef7bb
Jonah Walton <jwalton5@sheffield.ac.uk>
2023-12-02 19:11:44 +0000

    Refactored the Adding of new products into two classes

--- main/AddProductPopup.java
+++ main/AddProductPopup.java
@@ -4,12 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddProductPopup extends JFrame {
-    private AddTrainSet parentScreen;
+    private AddProduct parentScreen;
     private JTextArea productCodeTextArea = new JTextArea();
     private JTextArea quantityTextArea = new JTextArea();
     private ProductType productType;
 
-    public AddProductPopup(AddTrainSet parentScreenInit, ProductType productTypeInit) {
+    public AddProductPopup(AddProduct parentScreenInit, ProductType productTypeInit) {
         this.parentScreen = parentScreenInit;
         this.productType = productTypeInit;
         setTitle("Train Set");
@@ -43,6 +43,10 @@ public class AddProductPopup extends JFrame {
         }
 
         if (productType == ProductType.TRACKPACK){
+            detailsPanel.add(new JLabel("productCodeTextArea (Product Code should start with 'P'):"));
+        }
+
+        if (productType == ProductType.TRACK){
             detailsPanel.add(new JLabel("productCodeTextArea (Product Code should start with 'P'):"));
         }
 
